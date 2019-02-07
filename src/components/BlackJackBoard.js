c1442a71575e1f24102ff786cf12f3e03ede6024
krestko <Katherine.V.Restko@gmail.com>
2019-02-07 04:03:05 -0600

    Complete functionality for first round deal in blackjack app

--- src/components/BlackJackBoard.js
+++ src/components/BlackJackBoard.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 
 class BlackJackBoard extends Component {
+  state = {
+    toggleTurn: true
+  }
 
-  componentDidMount() {
-    let holdDealer = [];
-    holdDealer.push(this.props.handleDeal());
-    holdDealer.push(this.props.handleDeal());
-    this.props.handleDealerHand(holdDealer);
-    let holdPlayer = [];
-    holdPlayer.push(this.props.handleDeal());
-    holdPlayer.push(this.props.handleDeal());
-    this.props.handlePlayerHand(holdPlayer);
+  handleDeal = (e) => {
+    if(this.props.dealerHand.length === 2) {
+      return null;
+    } else if(this.state.toggleTurn) {
+      let holdPlayer = [];
+      holdPlayer.push(this.props.handleDeal());
+      this.props.handlePlayerHand(holdPlayer);
+      this.setState({ toggleTurn: false })
+    } else {
+      let holdDealer = [];
+      holdDealer.push(this.props.handleDeal());
+      this.props.handleDealerHand(holdDealer);
+      this.setState({ toggleTurn: true })
+    }
   }
 
   render() {
+    console.log(this.props.deck)
+    console.log('D', this.props.dealerHand)
+    console.log('P', this.props.playerHand)
+    console.log(this.state.dealCounter)
 
     return (
       <div className="App">
+        <button onClick={this.handleDeal}>Play</button>
       </div>
     );
   }
--- src/pages/BlackJackPage.js
+++ src/pages/BlackJackPage.js
@@ -3,32 +3,39 @@ import BlackJackBoard from '../components/BlackJackBoard'
 
 class BlackJackPage extends Component {
   state = {
-    deckNumCards: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'],
-    deckFaceCards: ['hearts', 'diamonds', 'clubs', 'spades'],
-    dealerHand: null,
-    playerHand: null
+    deck: [{2: 'hearts'}, {3: 'hearts'}, {4: 'hearts'}, {5: 'hearts'}, {6: 'hearts'}, {7: 'hearts'}, {8: 'hearts'}, {9: 'hearts'}, {10: 'hearts'}, {'jack': 'hearts'}, {'queen': 'hearts'}, {'king': 'hearts'}, {'ace': 'hearts'}, {2: 'diamonds'}, {3: 'diamonds'}, {4: 'diamonds'}, {5: 'diamonds'}, {6: 'diamonds'}, {7: 'diamonds'}, {8: 'diamonds'}, {9: 'diamonds'}, {10: 'diamonds'}, {'jack': 'diamonds'}, {'queen': 'diamonds'}, {'king': 'diamonds'}, {'ace': 'diamonds'}, {2: 'clubs'}, {3: 'clubs'}, {4: 'clubs'}, {5: 'clubs'}, {6: 'clubs'}, {7: 'clubs'}, {8: 'clubs'}, {9: 'clubs'}, {10: 'clubs'}, {'jack': 'clubs'}, {'queen': 'clubs'}, {'king': 'clubs'}, {'ace': 'clubs'}, {2: 'spades'}, {3: 'spades'}, {4: 'spades'}, {5: 'spades'}, {6: 'spades'}, {7: 'spades'}, {8: 'spades'}, {9: 'spades'}, {10: 'spades'}, {'jack': 'spades'}, {'queen': 'spades'}, {'king': 'spades'}, {'ace': 'spades'}],
+    dealerHand: [],
+    playerHand: []
+  }
+
+  handleRemoveCard = (card) => {
+    const newDeck = this.state.deck.filter((deckCard, index) => index !== card) 
+    this.setState({ deck: newDeck })
   }
 
   handleDeal = () => {
-    let numCards = [];
-    numCards.push(this.state.deckNumCards[Math.floor(Math.random() * this.state.deckNumCards.length)]);
-    numCards.push(this.state.deckFaceCards[Math.floor(Math.random() * this.state.deckFaceCards.length)]);
-    return numCards;
+    let card = [];
+    let holdCard = this.state.deck[Math.floor(Math.random() * this.state.deck.length)];
+    let indexHoldCard = this.state.deck.findIndex(card => card === holdCard)
+    card.push(holdCard);
+    this.handleRemoveCard(indexHoldCard)
+    return card;
   }
 
   handleDealerHand = (cards) => {
-    this.setState({ dealerHand: cards })
+    this.setState({ dealerHand: [...this.state.dealerHand, cards] })
   }
 
   handlePlayerHand = (cards) => {
-    this.setState({ playerHand: cards })
+    this.setState({ playerHand: [...this.state.playerHand, cards] })
   }
 
   render() {
+
     return (
       <div className="App">
         <h1>Blackjack</h1>
-        <BlackJackBoard deckNumCards={this.state.deckNumCards} deckFaceCards={this.state.deckFaceCards} dealerHand={this.state.dealerHand} playerHand={this.state.playerHand} handleDeal={this.handleDeal} handleDealerHand={this.handleDealerHand} handlePlayerHand={this.handlePlayerHand}/>
+        <BlackJackBoard deck={this.state.deck} dealerHand={this.state.dealerHand} playerHand={this.state.playerHand} handleDeal={this.handleDeal} handleDealerHand={this.handleDealerHand} handlePlayerHand={this.handlePlayerHand}/>
       </div>
     );
   }
